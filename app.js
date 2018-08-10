fc06a0a411e7a7ccc91d9de7fe10a4f55ffadaea
pavankalyankolla <pkalyan097@gmail.com>
2018-08-10 10:14:06 +0530

    initial commmit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+//custom logger middleware
+// app.use((req,res,next) => {
+//     console.log(`${req.method} - ${req.url} - ${req.ip} - ${new Date()}`);
+// });
+
+//Route Handler
+// app.Method(Path,Handler)
+
+app.get('/',(req,res) => {
+    res.send({
+        msg : 'Welcome to ticket master'
+    });
+});
+
+
+app.listen(port,() => {
+    console.log('listening port',port);
+})
