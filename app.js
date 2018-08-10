02a0c08b52bcd01a18d6a6f6199be6545d93ad06
pavankalyankolla <pkalyan097@gmail.com>
2018-08-10 12:05:07 +0530

    schema-get-post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('./config/db');
 
+const { Ticket } = require('./models/ticket');
+
 const app = express();
 const port = 3000;
 //Middlewares
@@ -22,6 +24,35 @@ app.get('/',(req,res) => {
         msg : 'Welcome to ticket master'
     });
 });
+
+app.get('/tickets',(req,res) => {
+    Ticket.find()
+    .then((tickets) => {
+        res.send(tickets);
+    })
+    .catch((err) => {
+        res.send(err);
+    }) 
+});
+
+app.post('/tickets',(req,res) => {
+    let body = req.body;
+    let ticket = new Ticket(body);
+    ticket.save().then((ticket) => {
+        res.send(ticket);
+    }).catch((err) => {
+        res.send(err);
+    })
+});
+
+app.get('/tickets/:id',(req,res) => {
+    let id =req.params.id;
+    Ticket.findById(id).then((ticket) => {
+        res.send(ticket);
+    }) .catch((err) => {
+        res.send(err);
+    })
+}); 
 
 
 app.listen(port,() => {
