999f8315cb2c35db189d271ebeecc1bf8aeee526
pavankalyankolla <pkalyan097@gmail.com>
2018-08-10 10:54:35 +0530

    db config

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('./config/db');
+
 const app = express();
 const port = 3000;
-
-const mongoose = require('mongoose');
 //Middlewares
 app.use(bodyParser.json());
 app.use(morgan('dev'));
