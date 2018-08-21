7506a4a96d96289fa02583776463d619917abcae
pavankalyankolla <pkalyan097@gmail.com>
2018-08-21 12:48:52 +0530

    ticket-router

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const mongoose = require('./config/db');
 
 const { Ticket } = require('./models/ticket');
 
+const { ticketRouter } = require('./routes/ticket');
+
 const app = express();
 const port = 3000;
 //Middlewares
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+app.use('/tickets',ticketRouter);
 
 app.param('id',(req,res,next) => {
  let id = req.params.id;
@@ -42,97 +46,7 @@ app.param('id',(req,res,next) => {
 //Route Handler
 // app.Method(Path,Handler)
 
-app.get('/',(req,res) => {
-    res.send({
-        msg : 'Welcome to ticket master'
-    });
-});
 
-app.get('/tickets',(req,res) => {
-    Ticket.find()
-    .then((tickets) => {
-        res.send(tickets);
-    })
-    .catch((err) => {
-        res.send(err);
-    }) 
-});
-
-app.post('/tickets',(req,res) => {
-    // let body = req.body;
-    // strong parameter check
-     
-    let body = _.pick(req.body,['name','department','message','priority']);
-
-    let ticket = new Ticket(body);
-    ticket.save().then((ticket) => {
-        res.send(ticket);
-    }).catch((err) => {
-        res.send(err);
-    })
-});
-
-app.get('/tickets/:id',(req,res) => {
-    let id = req.params.id;
-
-    Ticket.findById(id).then((ticket) => {
-       if(ticket){
-        res.send(ticket);
-       } else {
-           res.send({
-               notice : 'Ticket not found'
-           })
-       }
-       
-    }) .catch((err) => {
-        res.send(err);
-    })
-}); 
-
-app.put('/tickets/:id',(req,res) => {
-    let id = req.params.id;
-
-    let body = _.pick(req.body,['name','department','message','priority','status']);
-    Ticket.findByIdAndUpdate(id,{ $set: body },{ new : true})
-    .then((ticket) => {
-        if(ticket) {
-            res.send({
-                ticket,
-                notice : 'Successfully updated the record'
-            });
-        }   else{
-            res.send({
-                notice : 'Ticket not found'
-            })
-        }
-       
-    })
-    .catch((err) => {
-        res.send(err);
-    })
-});
-
-app.delete('/tickets/:id',(req,res) => {
-    let id = req.params.id;
-
-    Ticket.findByIdAndRemove(id) 
-    .then((ticket) => {
-      if(ticket){
-        res.send({
-            ticket,
-            notice:'sucessfully deleted'
-        })
-      } 
-      else{
-          res.send({
-              notice : 'Ticket not found'
-          });
-      }
-    })
-    .catch((err) => {
-        res.send(err);  
-    })
-})
 
 
 app.listen(port,() => {
---
+++ routes/ticket.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const { Ticket } = require('../models/ticket');
+const router = express.Router();
+const _ = require('lodash');
+
+
+// router.get('/',(req,res) => {
+//     res.send({
+//         msg : 'Welcome to ticket master'
+//     });
+// });
+
+router.get('/',(req,res) => {
+    Ticket.find()
+    .then((tickets) => {
+        res.send(tickets);
+    })
+    .catch((err) => {
+        res.send(err);
+    }) 
+});
+
+router.post('/',(req,res) => {
+    // let body = req.body;
+    // strong parameter check
+     
+    let body = _.pick(req.body,['name','department','message','priority']);
+
+    let ticket = new Ticket(body);
+    ticket.save().then((ticket) => {
+        res.send(ticket);
+    }).catch((err) => {
+        res.send(err);
+    })
+});
+
+router.get('/:id',(req,res) => {
+    let id = req.params.id;
+
+    Ticket.findById(id).then((ticket) => {
+       if(ticket){
+        res.send(ticket);
+       } else {
+           res.send({
+               notice : 'Ticket not found'
+           })
+       }
+       
+    }) .catch((err) => {
+        res.send(err);
+    })
+}); 
+
+router.put('/:id',(req,res) => {
+    let id = req.params.id;
+
+    let body = _.pick(req.body,['name','department','message','priority','status']);
+    Ticket.findByIdAndUpdate(id,{ $set: body },{ new : true})
+    .then((ticket) => {
+        if(ticket) {
+            res.send({
+                ticket,
+                notice : 'Successfully updated the record'
+            });
+        }   else{
+            res.send({
+                notice : 'Ticket not found'
+            })
+        }
+       
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
+
+router.delete('/:id',(req,res) => {
+    let id = req.params.id;
+
+    Ticket.findByIdAndRemove(id) 
+    .then((ticket) => {
+      if(ticket){
+        res.send({
+            ticket,
+            notice:'sucessfully deleted'
+        })
+      } 
+      else{
+          res.send({
+              notice : 'Ticket not found'
+          });
+      }
+    })
+    .catch((err) => {
+        res.send(err);  
+    })
+})
+
+module.exports = {
+   ticketRouter : router
+}
